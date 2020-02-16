7d51cf7f6f40c009788085891814c2c187b65aee
Allen <asd9fgh9@gmail.com>
2020-02-16 22:36:10 +0800

    登入表單驗證(同註冊表單驗證)

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,7 @@ var firebase = require('../connections/firebase_connect');
 var firebaseDb = require('../connections/firebase_admin_connect')
 var fireAuth = firebase.auth();
 router.get('/', function (req, res) {
-    res.render('login', { title: '登入' });
+    res.render('login', { title: '登入',error: req.flash('error')});
 })
 router.post('/', function (req, res) {
     fireAuth.signInWithEmailAndPassword(req.body.email,req.body.passwd)
@@ -14,9 +14,22 @@ router.post('/', function (req, res) {
         console.log('登入成功');
     })
     .catch(function(error){
-        res.redirect('/');
+        var errorMessage = error.message;
+        switch(errorMessage) {
+            case 'The email address is badly formatted.':
+                errorMessage = '電子郵件地址格式錯誤';
+                break;
+            case 'There is no user record corresponding to this identifier. The user may have been deleted.':
+                errorMessage = '查無此信箱';
+                break;  
+            case 'The password is invalid or the user does not have a password.':
+                errorMessage = '密碼錯誤';
+                break;  
+        }
         console.log(error);
         console.log('登入失敗');
+        req.flash('error',errorMessage);
+        res.redirect('/login')
     })
 })
 module.exports = router;
