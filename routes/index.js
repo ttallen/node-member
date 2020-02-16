74141246a1d06b8931bc0d2e6704a3bc1a2e0840
Allen <asd9fgh9@gmail.com>
2020-02-16 23:01:17 +0800

    登出邏輯設計

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var firebaseDb = require('../connections/firebase_admin_connect');
 var firebase = require('../connections/firebase_connect');
+var fireAuth = firebase.auth();
 router.get('/', function (req, res, next) {
     firebaseDb.ref('list').once('value',function(snapshot){
         var auth = req.session.uid;
@@ -12,6 +13,18 @@ router.get('/', function (req, res, next) {
             list: snapshot.val()
         });
     })
+});
+
+router.post('/', (req, res) => {
+    fireAuth.signOut()
+      .then(() => {
+        console.log('登出成功');
+        req.session.uid = null;
+        res.redirect('/');
+      })
+      .catch((error) => {
+        console.log('登出錯誤', error.message);
+      });
 });
 /* GET home page. */
 module.exports = router;
