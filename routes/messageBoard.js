2557182fd419691a37f4ddf3d02aa8b069b56838
Allen <asd9fgh9@gmail.com>
2020-02-16 21:50:21 +0800

    留言板資料庫設計

--- routes/messageBoard.js
+++ routes/messageBoard.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var firebaseDb = require('../connections/firebase_admin_connect');
 router.post('/', function (req, res) {
-   
+    firebaseDb.ref('user/'+req.session.uid).once('value',function(snapshot){
+        var nickname = snapshot.val().nickname;
+        var ref = firebaseDb.ref('list').push();
+        var listConent = {
+            nickname: nickname,
+            content: req.body.content
+        }
+        ref.set(listConent)
+        .then(function(){
+            res.redirect('/');
+        })
+    })
 })
 module.exports = router;
