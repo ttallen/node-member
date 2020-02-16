3d3ccc180fa35ce402b3dd26bdd70d0caa0b977c
Allen <asd9fgh9@gmail.com>
2020-02-16 16:41:04 +0800

    載入認證模組

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var firebaseDb = require('../connections/firebase_admin_connect');
+var firebase = require('../connections/firebase_connect');
 router.get('/', function (req, res, next) {
+    console.log(firebase.auth());
     firebaseDb.ref().once('value',function(snapshot){
         console.log(snapshot.val());
     })
