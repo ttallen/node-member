f3cca8ec24081ab0bd6759ef8dc34b79c6217a65
Allen <asd9fgh9@gmail.com>
2020-02-16 19:35:25 +0800

    登入邏輯+會員session 設計

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,10 @@ var router = express.Router();
 var firebaseDb = require('../connections/firebase_admin_connect');
 var firebase = require('../connections/firebase_connect');
 router.get('/', function (req, res, next) {
-    console.log(firebase.auth());
-    firebaseDb.ref().once('value',function(snapshot){
-        console.log(snapshot.val());
-    })
+    var auth = req.session.uid;
     res.render('index', {
-        title: '六角學院留言板'
+        title: '留言板',
+        auth: auth
     });
 });
 /* GET home page. */
--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('../connections/firebase_connect');
+var firebaseDb = require('../connections/firebase_admin_connect')
+var fireAuth = firebase.auth();
 router.get('/', function (req, res) {
     res.render('login', { title: '登入' });
 })
 router.post('/', function (req, res) {
-    
+    fireAuth.signInWithEmailAndPassword(req.body.email,req.body.passwd)
+    .then(function(user){
+        req.session.uid = user.uid;
+        res.redirect('/');
+        console.log('登入成功');
+    })
+    .catch(function(error){
+        res.redirect('/');
+        console.log(error);
+        console.log('登入失敗');
+    })
 })
 module.exports = router;
